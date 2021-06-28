cdef448597a5d94e27fcee1fd9c3778009dad795
Sam Bailey <sam.bailey@live.co.uk>
2021-06-28 08:43:48 +0100

    add units and button to toggle them

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-const key = "YOUR API KEY HERE";
-
+const key = "";
+let units = "metric";
+let locale = "london"
 
 // function getWeatherData(location) {
 //   let weatherData = fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${key}`, {
@@ -12,7 +13,11 @@ const key = "YOUR API KEY HERE";
 const userInput = document.getElementById("search");
 const submitBtn = document.getElementById("submit");
 
-submitBtn.addEventListener("click", () => getWeatherData(userInput.value));
+submitBtn.addEventListener("click", () => {
+  locale = userInput.value;
+  getWeatherData(locale);
+});
+
 userInput.addEventListener("keyup", function(event) {
     console.log(event.code);
     if (event.code === "Enter") {
@@ -22,7 +27,7 @@ userInput.addEventListener("keyup", function(event) {
 });
 
 async function getWeatherData(location) {
-  const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${key}`, {
+  const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&units=${units}&appid=${key}`, {
     mode: 'cors'
   })
   const weatherData = await response.json();//return resolved response as json
@@ -35,7 +40,7 @@ async function getWeatherData(location) {
 
 // console.log(getWeatherData("london"));
 
-getWeatherData("london");
+getWeatherData(locale);
 
 // console.log(getWeatherData("london")); // this returns undefined because it doesnt wait for the promise
 // getWeatherData("berlin");
@@ -74,8 +79,30 @@ function displayData(weatherDataObject) {
   placeName.innerHTML = weatherDataObject.name;
   icon.src = `https://openweathermap.org/img/wn/${weatherDataObject.icon}@2x.png`;
   weatherType.innerHTML = weatherDataObject.word;
-  temp.innerHTML = weatherDataObject.temp;
-  feelsLike.innerHTML = weatherDataObject.feels_like;
-  wind.innerHTML = weatherDataObject.wind;
+  temp.innerHTML = `${weatherDataObject.temp} &#176 C`;
+  feelsLike.innerHTML = `${weatherDataObject.feels_like} &#176 C`;
+  wind.innerHTML = `${displayWindUnits(weatherDataObject)} mph`;
+}
+
+function displayWindUnits(weatherDataObject) {
+  let windUnits = "";
+  if (units === "metric") {
+    windUnits = `${Math.round(weatherDataObject.wind * 2.237)}`
+  } else {
+    windUnits = Math.round(weatherDataObject.wind);
+  }
+  return windUnits;
+}
+
+const unitsBtn = document.getElementById("changeUnits");
+unitsBtn.addEventListener("click", () => changeUnits());
+
+function changeUnits() {
+  if (units === "metric") {
+    units = "imperial"
+  } else {
+    units = "metric"
+  }
+  getWeatherData(locale);
 }
 
